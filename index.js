a8115f1fca3b69fabba0ac89f7610d6892cb032c
Gaby Cruz <gcruz.a10@gmail.com>
2021-07-24 16:16:42 -0500

    simplifying node files

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { Pool } = require('pg');
-const cool = require('cool-ascii-faces');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -27,7 +26,6 @@ express()
       res.send("Error " + err);
     }
   })
-  .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
