01271f4ba09e729c891e8dafc0487bd9d13f0023
Gaby Cruz <gcruz.a10@gmail.com>
2021-07-25 18:02:42 -0500

    add About Me section

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -4,10 +4,10 @@ import '../css/AppNavbar.css';
 export function AppNavbar() {
   return (
     <div className="innerContainer">
-      <Link to="/" className="button">Home</Link>
-      <Link to="/career" className="button">Career</Link>
-      <Link to="/interests" className="button">Interests</Link>
-      <Link to="/contact" className="button">Contact</Link>
+      <Link to="/" className="button button-font">Home</Link>
+      <Link to="/career" className="button button-font">Career</Link>
+      <Link to="/interests" className="button button-font">Interests</Link>
+      <Link to="/contact" className="button button-font">Contact</Link>
     </div>
   );
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,3 +1,13 @@
+import {Link} from "react-router-dom";
+
 export function Home() {
-  return <h1>Home</h1>;
+  return <>
+    <h1>About Me</h1>
+    <p>Hi there, I'm Gaby!</p>
+    <p>Born and raised in Houston, TX.</p>
+    <p>I initially thought I would major in Math, but changed my mind to purse Computer Science at the University of Houston. It was a very challenging four years, but I made it out alive with a useful skillset that would last me the rest of my life.</p>
+    <p>I'm currently a Software Engineer at <Link to="/career" className="pLinks">JPMorgan Chase</Link> working on some pretty cool things.</p>
+    <p>In my free time, I like to grow various types of <Link to="/interests" className="pLinks">cacti, practice hand-lettering, and eat good food.</Link></p>
+    <p>Hope you enjoy my personal website - I made it from scratch using ReactJS, Node, HTML5, & CSS3! 😃</p>
+  </>;
 }
