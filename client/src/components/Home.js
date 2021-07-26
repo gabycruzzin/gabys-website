6f982ac5900a252627eafdb7a8f34138e46b4082
Gaby Cruz <gcruz.a10@gmail.com>
2021-07-26 08:29:33 -0500

    update fonts and colors

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 export function Home() {
   return <>
     <h1>About Me</h1>
-    <p>Hi there, I'm Gaby!</p>
+    <p>Hi there, I'm Gaby! 🖖</p>
     <p>Born and raised in Houston, TX.</p>
     <p>I initially thought I would major in Math, but changed my mind to purse Computer Science at the University of Houston. It was a very challenging four years, but I made it out alive with a useful skillset that would last me the rest of my life.</p>
     <p>I'm currently a Software Engineer at <Link to="/career" className="pLinks">JPMorgan Chase</Link> working on some pretty cool things.</p>
