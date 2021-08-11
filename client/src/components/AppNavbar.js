b2ade0e669318ce6a131559736d6fdabbb176370
Gaby Cruz <gcruz.a10@gmail.com>
2021-08-11 18:51:26 -0500

    fix navbar and add hamburger menu

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -3,11 +3,18 @@ import '../css/AppNavbar.css';
 
 export function AppNavbar() {
   return (
-    <div className="innerContainer">
-      <Link to="/" className="button button-font">Home</Link>
-      <Link to="/journey" className="button button-font">Journey</Link>
-      <Link to="/interests" className="button button-font">Interests</Link>
-      <Link to="/contact" className="button button-font">Contact</Link>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup" style={{justifyContent: 'center'}}>
+        <div className="navbar-nav">
+          <Link to="/" className="button button-font">Home</Link>
+          <Link to="/journey" className="button button-font">Journey</Link>
+          <Link to="/interests" className="button button-font">Interests</Link>
+          <Link to="/contact" className="button button-font">Contact</Link>
+        </div>
+      </div>
+    </nav>
   );
 }
